edafb677a18cf0ea180fb12b44dfce34f9332b13
kmherman <72175052+kmherman@users.noreply.github.com>
2020-11-25 12:22:51 -0800

    Update test_prep_split_data.py

--- BioME/test_prep_split_data.py
+++ BioME/test_prep_split_data.py
@@ -12,16 +12,17 @@ class TestPrepSplitData(unittest.TestCase):
 
     def test_shot1(self):
         """
-        One-shot test on get_one_hot to check that one-hot encoding is correctly
-        performed.
+        One-shot test on get_one_hot to check that one-hot encoding is
+        correctly performed.
         """
-        self.assertEqual(get_one_hot(['cat', 'dog'], np.array([['cat'], ['dog']])),
+        self.assertEqual(get_one_hot(['cat', 'dog'],
+                                     np.array([['cat'], ['dog']])),
                          np.array([[1, 0], [0, 1]]))
 
     def test_edge1(self):
         """
-        Edge test on get_one_hot to check that ValueError is raised when label in
-        data is not present in list.
+        Edge test on get_one_hot to check that ValueError is raised
+        when label in data is not present in list.
         """
         with self.assertRaises(ValueError):
             get_one_hot(['cat', 'dog'], np.array([['cat'], ['kitten']]))
