d34928acf190375726e6e26d17a0b2dc3a727c16
kmherman <72175052+kmherman@users.noreply.github.com>
2020-11-25 12:18:28 -0800

    Update test_prep_split_data.py

--- BioME/test_prep_split_data.py
+++ BioME/test_prep_split_data.py
@@ -2,25 +2,26 @@ import unittest
 
 import numpy as np
 
-from prep_split_data import *
+from prep_split_data import get_one_hot
+
 
 class TestPrepSplitData(unittest.TestCase):
     """
-    H
+    Insert class documentation later
     """
-
 
     def test_shot1(self):
         """
-        One-shot test on get_one_hot to check that one-hot encoding is correctly performed.
+        One-shot test on get_one_hot to check that one-hot encoding is correctly
+        performed.
         """
-        self.assertEqual(pre_split_data.get_one_hot(['cat', 'dog'], np.array([['cat'], ['dog']])),
+        self.assertEqual(get_one_hot(['cat', 'dog'], np.array([['cat'], ['dog']])),
                          np.array([[1, 0], [0, 1]]))
-
 
     def test_edge1(self):
         """
-        Edge test on get_one_hot to check that ValueError is raised when label in data is not present in list.
+        Edge test on get_one_hot to check that ValueError is raised when label in
+        data is not present in list.
         """
         with self.assertRaises(ValueError):
-            prep_split_data.get_one_hot(['cat', 'dog'], np.array([['cat'], ['kitten']]))
+            get_one_hot(['cat', 'dog'], np.array([['cat'], ['kitten']]))
