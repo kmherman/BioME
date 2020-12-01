8465922ae5cc737fc77e948cb0f45a4aa3091dd8
kmherman <kmherman@uw.edu>
2020-12-01 12:36:31 -0800

    Add unittest test for prep_split_data.py

---
+++ BioME/test/test_prep_split_data.py
@@ -0,0 +1,93 @@
+"""
+Unittest module to test prep_split_data module..
+
+
+TestPrepSplitData(unittest.TestCase) -- class containing a smoke test,
+two one-shot tests, and two edge tests for prep_split_data.
+
+test_smoke(self) -- function tests that data_loader returns a tuple of arrays.
+
+test_shot1(self) -- function tests that get_one_hot returns correct one-hot
+encoding for a simple example.
+
+test_edge1(self) -- function tests that get_one_hot raises an exception when a
+categorical label in y_data is not contained in specified category list.
+
+test_shot2(self) -- function tests that split_train_test returns split arrays
+of appropriate sizes (90/10 train/test).
+
+test_edge2(self) -- function tests that split_train_test raises an exception
+when the x and y data have a different number of samples.
+"""
+import unittest
+
+import numpy as np
+
+from prep_split_data import data_loader
+from prep_split_data import get_one_hot
+from prep_split_data import split_train_test
+
+
+class TestPrepSplitData(unittest.TestCase):
+    """
+    Class to test functions in module prep_split_data.
+    """
+
+    def test_smoke(self):
+        """
+        Smoke test on data_loader to ensure that a tuple (of two arrays)
+        is returned.
+        """
+        self.assertTrue(isinstance(data_loader('../bug_OTU_rel.tsv',
+                                               '../FecesMeta.txt'), tuple))
+
+    def test_shot1(self):
+        """
+        One-shot test on get_one_hot to check that one-hot encoding is
+        correctly performed.
+        """
+        self.assertTrue((get_one_hot(['cat', 'dog'], np.array([['cat'],
+                                                               ['dog']]))
+                         == np.array([[1, 0], [0, 1]])).all())
+
+    def test_edge1(self):
+        """
+        Edge test on get_one_hot to check that ValueError is raised when label
+        in data is not present in list.
+        """
+        with self.assertRaises(ValueError):
+            get_one_hot(['cat', 'dog'], np.array([['kitten']]))
+
+    def test_shot2(self):
+        """
+        One-shot test on split_train_test to ensure 90/10 split of data.
+        """
+        xtrain, xtest, ytrain, ytest = split_train_test(np.array([[0], [1],
+                                                                  [2], [3],
+                                                                  [4], [5],
+                                                                  [6], [7],
+                                                                  [8], [9]]),
+                                                        np.array([[0], [1],
+                                                                  [2], [3],
+                                                                  [4], [5],
+                                                                  [6], [7],
+                                                                  [8], [9]]))
+        self.assertTrue(np.size(ytrain, axis=0) == 9)
+        self.assertTrue(np.size(xtrain, axis=0) == 9)
+        self.assertTrue(np.size(ytest, axis=1) == 1)
+        self.assertTrue(np.size(xtest, axis=1) == 1)
+
+    def test_edge2(self):
+        """
+        Edge test on split_train_test to check that exception is raised when
+        x_data and y_data do not have the same number of samples.
+        """
+        with self.assertRaises(IndexError):
+            split_train_test(np.array([[0], [1], [2], [3], [4], [5], [6], [7],
+                                       [8], [9]]),
+                             np.array([[0], [1], [2], [3], [4], [5], [6], [7],
+                                       [8]]))
+
+
+if __name__ == '__main__':
+    unittest.main()
