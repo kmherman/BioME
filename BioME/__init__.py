7b6416e2e6aa82cf550c85f869df8eb143ffad8a
kmherman <72175052+kmherman@users.noreply.github.com>
2020-12-03 14:22:34 -0800

    Update __init__.py

--- BioME/__init__.py
+++ BioME/__init__.py
@@ -1,3 +1,5 @@
 from .prep_split_data import data_loader
 from .prep_split_data import get_one_hot
 from .prep_split_data import split_train_test
+
+__all__ = [data_loader, get_one_hot, split_train_test]
