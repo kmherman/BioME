b4abbfe9b81c8dc4bc4cdea8d55d4423f07c49b5
Sierra Gillman <52217109+sjgillman@users.noreply.github.com>
2020-12-05 19:15:04 -0800

    made flake8 compliant

--- BioME/SVC.py
+++ BioME/SVC.py
@@ -19,6 +19,7 @@ model.
 """
 
 def get_SVC(x_train, y_train):
+    
     """
     This method trains and fits a Support Vector Classifier model.
 
@@ -39,24 +40,25 @@ def get_SVC(x_train, y_train):
     model : A trained and fitted SVC model.
 
     """
+    
     if isinstance(x_train, numpy.ndarray) is False or \
-        isinstance(y_train, numpy.ndarray) is False:
+       isinstance(y_train, numpy.ndarray) is False:
         raise TypeError("Training data must be numpy arrays.")
-    x_shape = numpy.shape(x_train)
-    y_shape = numpy.shape(y_train)
+    x_shape= numpy.shape(x_train)
+    y_shape= numpy.shape(y_train)
     if x_shape[0] != y_shape[0]:
         raise ValueError("The number of rows in the X training data must be \
                          equal to the length of the Y training data vector. \
-                         \n Rows in X: " + str(x_shape[0]) + \
+                         \n Rows in X: " + str(x_shape[0]) +
                          "\n Length of Y: " + str(y_shape[0]))
-    model = SVC(kernel='rbf', probability = True)
-    param_grid = {'C': [0.1, 1, 10, 100], 'gamma': [1, 0.1, 0.01, 0.001, \
+    model= SVC(kernel='rbf', probability= True)
+    param_grid= {'C': [0.1, 1, 10, 100], 'gamma': [1, 0.1, 0.01, 0.001,
                                                     0.00001, 10]}
-    clf_grid = sklearn.model_selection.GridSearchCV(model, \
+    clf_grid= sklearn.model_selection.GridSearchCV(model,
                                                     param_grid, verbose=1)
     clf_grid.fit(x_train, y_train)
-    model = SVC(C=clf_grid.best_params_['C'], kernel='rbf', \
-                gamma = clf_grid.best_params_['gamma'])
+    model= SVC(C=clf_grid.best_params_['C'], kernel='rbf',
+                gamma= clf_grid.best_params_['gamma'])
     return model
 
 
