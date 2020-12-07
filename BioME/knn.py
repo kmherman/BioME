6fdccb398d9ecda8a50d238d70226491030bc291
David Lee <davidlee92@gmail.com>
2020-12-06 17:09:33 -0800

    made flake8 compliant

--- BioME/knn.py
+++ BioME/knn.py
@@ -1,6 +1,6 @@
-import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.neighbors import KNeighborsClassifier
+
 
 def knn(x_train, y_train):
     """
@@ -13,18 +13,9 @@ def knn(x_train, y_train):
     """
     pca = PCA(n_components=2)
     X_train = pca.fit_transform(x_train)
-    X_test = pca.transform(x_test)
     '# Create KNN classifier'
     N = int(input("enter number of neighbors: "))
     knn = KNeighborsClassifier(n_neighbors=N)
     '# Fit the classifier to the data'
     model = knn.fit(X_train, y_train)
-
-    '# shows model predictions on the test data'
-    #knn.predict(X_test)
-
-    '# check accuracy of our model on the test data'
-    #knn.score(X_test, y_test)
-    
     return model
-
