fc9896896b2936f2070ca7a16973ce19eee965ae
kmherman <kmherman@uw.edu>
2020-12-08 13:51:31 -0800

    change imports

--- biome/test/test_naive_bayes.py
+++ biome/test/test_naive_bayes.py
@@ -7,10 +7,10 @@ Created on Mon Dec  7 13:14:46 2020
 
 
 from sklearn.datasets import load_iris
-from ..biome import prep_split_data as psd
-import naive_bayes as nb
+import biome
+import biome.naive_bayes as nb
 
 x, y = load_iris(return_X_y=True)
-x_train, x_test, y_train, y_test = psd.split_train_test(x, y)
+x_train, x_test, y_train, y_test = biome.split_train_test(x, y)
 
 model = nb.naive_bayes(x_train, y_train)
