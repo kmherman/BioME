1b0f2235ddbc0fe99eb8f6c7ca37f5a08b0ddc74
really-lilly <tatkalillian@gmail.com>
2020-12-08 15:02:05 -0800

    make flake8 compliant

--- biome/SVC.py
+++ biome/SVC.py
@@ -1,18 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 30 14:29:42 2020
-
-@author: LT
-"""
-import numpy as np
-from sklearn.svm import SVC
-import sklearn.model_selection
-
-## This will need data_ loader and split_train_test to work
-
-
-
-
 """
 This module contains functions to train a supporter vector classifier (SVC)
 model.
@@ -22,6 +7,9 @@ model.
 """
 
 
+import numpy as np
+from sklearn.svm import SVC
+import sklearn.model_selection
 
 
 def get_SVC(x_train, y_train):
@@ -48,26 +36,25 @@ def get_SVC(x_train, y_train):
     # Reshape y_data into vector:
     y_train = y_train.ravel()
     # Check data type:
-    if isinstance(x_train, np.ndarray) == False or \
-        isinstance(y_train, np.ndarray) == False:
+    if isinstance(x_train, np.ndarray) is False or \
+            isinstance(y_train, np.ndarray) is False:
         raise TypeError("Training data must be numpy arrays.")
     # Check shape:
     x_shape = np.shape(x_train)
     y_shape = np.shape(y_train)
     if x_shape[0] != y_shape[0]:
-        raise ValueError("The number of rows in the X training data must be \
-                         equal to the length of the Y training data vector. \
-                         \n Rows in X: " + str(x_shape[0]) + \
+        raise ValueError("The number of rows in the X training data must be"
+                         "equal to the length of the Y training vector."
+                         "\n Rows in X: " + str(x_shape[0]) +
                          "\n Length of Y: " + str(y_shape[0]))
     # Search and select best model parameters:
-    model = SVC(kernel='rbf', probability = True)
-    param_grid = {'C': [0.1, 1, 10, 100], 'gamma': [1, 0.1, 0.01, 0.001, \
+    model = SVC(kernel='rbf', probability=True)
+    param_grid = {'C': [0.1, 1, 10, 100], 'gamma': [1, 0.1, 0.01, 0.001,
                                                     0.00001, 10]}
-    clf_grid = sklearn.model_selection.GridSearchCV(model, \
+    clf_grid = sklearn.model_selection.GridSearchCV(model,
                                                     param_grid, verbose=1)
     # Train model:
     clf_grid.fit(x_train, y_train)
-    model = SVC(C=clf_grid.best_params_['C'], kernel='rbf', \
-                gamma = clf_grid.best_params_['gamma'])
+    model = SVC(C=clf_grid.best_params_['C'], kernel='rbf',
+                gamma=clf_grid.best_params_['gamma'])
     return model
-
