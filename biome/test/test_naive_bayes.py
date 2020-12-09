3e0bfdb130e7cb24fd101d0761ba95a93c089b43
really-lilly <tatkalillian@gmail.com>
2020-12-09 14:55:54 -0800

    make test_naive_bayes.py flake8 compliant for real and this time I mean it.

--- biome/test/test_naive_bayes.py
+++ biome/test/test_naive_bayes.py
@@ -1,75 +1,54 @@
-# -*- coding: utf-8 -*-
 """
-Created on Mon Dec  7 13:14:46 2020
+This module contains unit tests for the naive_bayes.py module
+"""
 
-@author: user
-"""
 
 import unittest
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_iris  # A sample dataset for classification
 import biome.test_prep_split_data as tsp
-#TODO fix this import
-#from biome. import naive_bayes as nb
-
-# Have they used any code that uses "import biome" since biome.py was added
-# to the directory? Becauase when importing, it just runs the module and 
-# prints out the ASCII label due to name ambiguity
+from biome import naive_bayes as nb
 
 
-#x, y = load_iris(return_X_y=True)
-#x_train, x_test, y_train, y_test = biome.split_train_test(x, y)
+class UnitTests(unittest.TestCase):
+
+    x, y = load_iris(return_X_y=True)
+    x_train, x_test, y_train, y_test = tsp(x, y)
+    y_pred = [2, 1, 0, 0, 2, 1, 1, 2, 0, 2, 1, 1, 1, 1, 1]
+    model = nb.get_GNB(x_train, y_train)
+
+    def test_data_type(self):
+        """
+        This method tests that an error is thrown if data is incorrect type.
+        """
+        x_train = "A string"
+        model = nb.GNB()
+        with self.assertRaises(TypeError):
+            model.get_GNB(x_train, UnitTests.y_train)
+
+    def test_data_size(self):
+        """
+        This method tests that an error is thrown if data is incorrect size.
+        """
+        y_train = UnitTests.y_train[2:]  # Make y_train too short
+        model = nb.GNB()
+        with self.assertRaises(ValueError):
+            model.get_GNB(UnitTests.x_train, y_train, UnitTests.x_test,
+                          UnitTests.y_test)
+
+    def test_predict(self):
+        """
+        This method tests that a trained model is mostly able to correctly
+        classify data.
+        """
+        model = nb.GNB()
+        model.get_GNB(UnitTests.x_train, UnitTests.y_train)
+        y_pred = model.predict(UnitTests.x_test)
+        n_incorrect = (UnitTests.y_test != y_pred).sum()
+        self.assertTrue(n_incorrect < 3)
 
 
+if __name__ == '__main__':
+    unittest.main()
 
-
-
-# class UnitTests(unittest.TestCase):
-    
-  
-
-#     y_pred = [2, 1, 0, 0, 2, 1, 1, 2, 0, 2, 1, 1, 1, 1, 1]
-#     model = biome.naive_bayes.get_GNB(x_train, y_train, x_test, y_test)
-    
-#     def test_smoke(self):
-#         """
-#         This method tests that the constructor makes the correct data type.
-#         """
-#         model = nb.GNB()
-#         self.assertTrue(isinstance(model, __main__.GNB))
-    
-#     def test_data_type(self):
-#         """
-#         This method tests that an error is thrown if data is incorrect type.
-#         """
-#         x_train = "A string"
-#         model = nb.GNB()
-#         with self.assertRaises(TypeError):
-#             model.get_GNB(x_train, y_train, x_test, y_test)
-    
-#     def test_data_size(self):
-#         """
-#         This method tests that an error is thrown if data is incorrect size.
-#         """
-#         y_train = y_train[2:] # Make y_train too short
-#         model = nb.GNB()
-#         with self.assertRaises(ValueError):
-#             model.get_GNB(x_train, y_train, x_test, y_test)
-    
-#     def test_predict(self):
-#         """
-#         This method tests that a trained model is mostly able to correctly
-#         classify data.
-#         """
-#         model = nb.GNB()
-#         model.get_GNB(x_train, y_train, x_test, y_test)
-        
-#         y_pred = model.predict(x_test)
-        
-#         n_incorrect = (y_test != y_pred).sum()
-#         self.assertTrue(n_incorrect < 3)
-
-# if __name__ == '__main__':
-#     unittest.main()
-
-# SUITE = unittest.TestLoader().loadTestsFromTestCase(UnitTests)
-# _ = unittest.TextTestRunner().run(SUITE)
+SUITE = unittest.TestLoader().loadTestsFromTestCase(UnitTests)
+_ = unittest.TextTestRunner().run(SUITE)
