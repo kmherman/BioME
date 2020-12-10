690c39390b19c2c7907582694feece5a4d7d86b9
Lilly Tatka <ltatka@uw.edu>
2020-12-10 14:53:33 -0800

    add GNB as a superclass to UnitTests

---
+++ biome/test_naive_bayes.py
@@ -0,0 +1,54 @@
+"""
+This module contains unit tests for the naive_bayes.py module
+"""
+
+
+import unittest
+from sklearn.datasets import load_iris  # A sample dataset for classification
+from biome import prep_split_data as psd
+from biome import naive_bayes as nb
+
+
+class UnitTests(unittest.TestCase):
+
+    x, y = load_iris(return_X_y=True)
+    x_train, x_test, y_train, y_test = psd.split_train_test(x, y)
+
+    model = nb.get_GNB(x_train, y_train)
+
+    def test_data_type(self):
+        """
+        This method tests that an error is thrown if data is incorrect type.
+        """
+        x_train = "A string"
+        model = nb.GNB()
+        with self.assertRaises(TypeError):
+            model.get_GNB(x_train, UnitTests.y_train)
+
+    def test_data_size(self):
+        """
+        This method tests that an error is thrown if data is incorrect size.
+        """
+        y_train = UnitTests.y_train[2:]  # Make y_train too short
+        model = nb.GNB()
+        with self.assertRaises(ValueError):
+            model.get_GNB(UnitTests.x_train, y_train, UnitTests.x_test,
+                          UnitTests.y_test)
+
+    def test_predict(self):
+        """
+        This method tests that a trained model is mostly able to correctly
+        classify data.
+        """
+        model = nb.GNB()
+        model.get_GNB(UnitTests.x_train, UnitTests.y_train)
+        y_pred = model.predict(UnitTests.x_test)
+        n_incorrect = (UnitTests.y_test != y_pred).sum()
+        self.assertTrue(n_incorrect < 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+SUITE = unittest.TestLoader().loadTestsFromTestCase(UnitTests)
+_ = unittest.TextTestRunner().run(SUITE)
