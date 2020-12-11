c65e975a333da05263d2b1702313301910ef2761
Lilly Tatka <ltatka@uw.edu>
2020-12-11 11:12:17 -0800

    naive_bayes and test fixed(?)

--- biome/test/test_naive_bayes.py
+++ biome/test/test_naive_bayes.py
@@ -5,21 +5,20 @@ This module contains unit tests for the naive_bayes.py module
 
 import unittest
 from sklearn.datasets import load_iris  # A sample dataset for classification
-#from biome import prep_split_data as psd
-#from biome import naive_bayes as nb
 import biome
 
-class UnitTests(unittest.TestCase, naive_bayes.GNB):
+
+class UnitTests(unittest.TestCase, biome.naive_bayes.GNB):
 
     x, y = load_iris(return_X_y=True)
-    x_train, x_test, y_train, y_test = psd.split_train_test(x, y)
+    x_train, x_test, y_train, y_test = biome.split_train_test(x, y)
 
     def test_data_type(self):
         """
         This method tests that an error is thrown if data is incorrect type.
         """
         x_train = "A string"
-        model = GNB(x_train, UnitTests.y_train)
+        model = biome.GNB(x_train, UnitTests.y_train)
         with self.assertRaises(TypeError):
             model.get_GNB(x_train, UnitTests.y_train)
 
@@ -28,16 +27,16 @@ class UnitTests(unittest.TestCase, naive_bayes.GNB):
         This method tests that an error is thrown if data is incorrect size.
         """
         y_train = UnitTests.y_train[2:]  # Make y_train too short
-        model = GNB()
+        model = biome.GNB(UnitTests.x_train, y_train)
         with self.assertRaises(ValueError):
-            model.get_GNB(UnitTests.x_train, y_train)
+            model.get_GNB()
 
     def test_predict(self):
         """
         This method tests that a trained model is mostly able to correctly
         classify data.
         """
-        model = GNB(UnitTests.x_train, UnitTests.y_train)
+        model = biome.GNB(UnitTests.x_train, UnitTests.y_train)
         model.get_GNB()
         y_pred = model.predict(UnitTests.x_test)
         n_incorrect = (UnitTests.y_test != y_pred).sum()
