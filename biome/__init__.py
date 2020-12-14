371fd11eccace3ac246614ac244a085317c47d6e
kmherman <72175052+kmherman@users.noreply.github.com>
2020-12-13 21:44:31 -0800

    Update __init__.py

--- biome/__init__.py
+++ biome/__init__.py
@@ -7,6 +7,7 @@ from .train_mlp import train_nn1
 from .train_mlp import train_nn3
 from .select_model import get_trained_models
 from .select_model import evaluate_rank_models
+from .select_model import get_prediction
 from .logistic import logistic_regress
 from .forest import random_forest
 from .naive_bayes import GNB
@@ -15,4 +16,4 @@ from .SVC import get_SVC
 __all__ = [data_loader, get_one_hot, split_train_test, forward_nn1,
            forward_nn3, train_nn1, train_nn3, get_trained_models,
            evaluate_rank_models, logistic_regress, random_forest,
-           GNB, get_SVC]
+           GNB, get_SVC, get_prediction]
