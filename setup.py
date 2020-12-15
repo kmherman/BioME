8a035986bdb7f2ce9538704dba18207e7df2085f
Sierra Gillman <52217109+sjgillman@users.noreply.github.com>
2020-12-15 11:11:15 -0800

    added arguments for script and installers

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ opts = dict(
         author='',
         author_email='kmherman@uw.edu',
         description='Supervised Machine Learning for Microbiome Data',
+        scripts=['biome/scripts/biome_run.py'],
+        install_requires=['torch','scikit-learn','numpy','pandas'],
         packages=find_packages()
         )
 
